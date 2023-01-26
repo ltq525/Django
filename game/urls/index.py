37d3849c2c0bcf9178f069cfa446949a6c422733
ltq525 <107291452+ltq525@users.noreply.github.com>
2023-01-26 17:23:49 +0800

    Update index.py

--- game/urls/index.py
+++ game/urls/index.py
@@ -4,7 +4,7 @@ from game.views.index import index
 urlpatterns = [
         path("", index, name="index"),
         path("menu/", include("game.urls.menu.index")),
-        path("palygronud/", include("game.urls.playground.index")),
+        path("playgronud/", include("game.urls.playground.index")),
         path("settings/", include("game.urls.settings.index")),
 ]
 
