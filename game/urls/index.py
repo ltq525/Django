131b0b5d1af67bd7542d893bcd163caa462b70c7
Liutingquan <1766445410@qq.com>
2023-01-26 16:44:14 +0800

    2.0

---
+++ game/urls/index.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from game.view.index import index
+
+urlpatterns = [
+        path("", index, name="index"),
+        path("menu/", include("game.urls.menu.index")),
+        path("palygronud/", include("game.urls.playground.index")),
+        path("settings/", include("game.urls.settings.index")),
+]
+
